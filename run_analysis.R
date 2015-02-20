5001e7c6dc49f5ee32b33a45492a225e7041c031
Wouter Ceulemans <wouter.ceulemans@antwerpen.be>
2015-02-20 10:28:48 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,62 +1,57 @@
-Enter file contents here
-## Download the training and test data sets
+################################################################################
+##   Getting and Cleaning Data Course Project
+##   February 20, 2015
+################################################################################
+
+
+## Download the training and test data sets and unzip
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile = "Dataset.zip")   ##, method = "curl") ## 
+download.file(fileUrl, destfile = "Dataset.zip" , method = "curl")
 unzip("Dataset.zip")
 
-## Import the data sets
-xTrain<-read.table("./UCI HAR Dataset/train/X_train.txt")
-xTest<-read.table("./UCI HAR Dataset/test/X_test.txt")
-yTrain<-read.table("./UCI HAR Dataset/train/y_train.txt")
-yTest<-read.table("./UCI HAR Dataset/test/y_test.txt")
-subjectTrain<-read.table("./UCI HAR Dataset/train/subject_train.txt")
-subjectTest<-read.table("./UCI HAR Dataset/test/subject_test.txt")
-activityLabels<-read.table("./UCI HAR Dataset/activity_labels.txt")
-featuresData<-read.table("./UCI HAR Dataset/features.txt")
 
-## Merges the training and the test sets to create one data set.
-xDataSet<-rbind(xTrain,xTest)
+## Importing  the data sets
+xtrain<-read.table("./UCI HAR Dataset/train/X_train.txt")
+xtest<-read.table("./UCI HAR Dataset/test/X_test.txt")
+ytrain<-read.table("./UCI HAR Dataset/train/y_train.txt")
+ytest<-read.table("./UCI HAR Dataset/test/y_test.txt")
+subjecttrain<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+subjecttest<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+activitylabels<-read.table("./UCI HAR Dataset/activity_labels.txt")
+features<-read.table("./UCI HAR Dataset/features.txt")
 
-## Extracts only the measurements on the mean and standard deviation for each measurement
-colnames(xDataSet) <- c(as.character(featuresData[,2]))
 
-Mean<-grep("mean()",colnames(xDataSet),fixed=TRUE)
-SD<-grep("std()",colnames(xDataSet),fixed=TRUE)
-MeanSD<-xDataSet[,c(Mean,SD)]
+## Merges the training and the test sets to create one data set for X 
+xdata<-rbind(xtrain,xtest)
 
-## Uses descriptive activity names to name the activities in the data set
-yDataSet<-rbind(yTrain,yTest)
 
-activityData<-cbind(yDataSet,MeanSD)
+## Extracts only the measurements on the mean and standard deviation
+colnames(xdata) <- c(as.character(features[,2]))
+mean<-grep("mean()",colnames(xdata),fixed=TRUE)
+stdev<-grep("std()",colnames(xdata),fixed=TRUE)
+meanstdev<-xdata[,c(mean,stdev)]
 
-colnames(activityData)[1] <- "Activity"
+## Combine the activity data with the dataset of means and standard deviations
+ydata<-rbind(ytrain,ytest)
+activitydata<-cbind(ydata,meanstdev)
+
 
 ## Appropriately labels the data set with descriptive activity names.
-## use  merge? or use for loop?
 
-activityLabels[,2]<-as.character(activityLabels[,2])
+colnames(activitydata)[1] <- "Activity"
 
-for(i in 1:length(activityData[,1])){
-  activityData[i,1]<-activityLabels[activityData[i,1],2]
+## Uses descriptive activity names to name the activities in the data set
+activitylabels[,2]<-as.character(activitylabels[,2])
+for(i in 1:length(activitydata[,1])){
+        activitydata[i,1]<-activitylabels[activitydata[i,1],2]
 }
-
-
 ## Creates a second, independent tidy data set with the average of each variable for each activity and each subject
-subjectData<-rbind(subjectTrain,subjectTest)
-
-allData<-cbind(subjectData,activityData)
-
-colnames(allData)[1] <- "Subject"
-
-tidyData <- aggregate( allData[,3] ~ Subject+Activity, data = allData, FUN = "mean" )
-
-for(i in 4:ncol(allData)){
-  tidyData[,i] <- aggregate( allData[,i] ~ Subject+Activity, data = allData, FUN = "mean" )[,3]
+subjectdata<-rbind(subjecttrain,subjecttest)
+alldata<-cbind(subjectdata,activitydata)
+colnames(alldata)[1] <- "Subject"
+tidydata <- aggregate( alldata[,3] ~ Subject+Activity, data = alldata, FUN = "mean" )
+for(i in 4:ncol(alldata)){
+        tidydata[,i] <- aggregate( alldata[,i] ~ Subject+Activity, data = alldata, FUN = "mean" )[,3]
 }
-
-colnames(tidyData)[3:ncol(tidyData)] <- colnames(MeanSD)
-
-write.table(tidyData, file = "tidyData.txt")
-
-
-## tidyDataSet <- read.table("./tidyData.txt") 
+colnames(tidydata)[3:ncol(tidydata)] <- colnames(meanstdev)
+write.table(tidydata, file = "tidydata.txt")
